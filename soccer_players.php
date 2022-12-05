463c03f8b650741586b86231aabf541ad423a211
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-12-05 11:24:40 -0600

    changes

---
+++ soccer_players.php
@@ -0,0 +1,62 @@
+<?php include 'header.php';?>
+
+    <h1>Players</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>PlayerID</th>
+      <th>FirstName</th>
+      <th>LastName</th>
+      <th>Club</th>
+      <th>Position</th>
+      <th>Nationality</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+$servername = "localhost";
+$username = "felixfin_user2";
+$password = "O-,GXdw4e3QG";
+$dbname = "felixfin_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['position'])) {
+  $var = $_POST['position'];
+  $sql = "SELECT * from SoccerPlayer where Position='$var'";
+}
+else {
+  $sql = "SELECT * from SoccerPlayer";
+}
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["PlayerID"]?></td>
+    <td><?=$row["FirstName"]?></td>
+    <td><?=$row["LastName"]?></td>
+    <td><?=$row["Club"]?></td>
+    <td><?=$row["Position"]?></td>
+    <td><?=$row["Nationality"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+
+<a class="btn btn-primary" type="button" href="index.php">Go Back</a>
+
+<?php include 'footer.php';?>

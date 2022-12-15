3ceb0e8c963f8e1f072c897a151683f6e32d6913
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-12-15 16:42:54 -0600

    change index to php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!-- <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -38,7 +38,10 @@
           </ul>
         </div>
       </div>
-    </nav>
+    </nav> -->
+
+    <?php include 'header.php';?>
+
     <img src="img/world_cup.jpg" class="img-fluid" alt="..." width="100%">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>

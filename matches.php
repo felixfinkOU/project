1225562c43e31d2926367eccf59181cbe57f8dd4
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-12-15 20:26:24 -0600

    sql fix

--- matches.php
+++ matches.php
@@ -32,7 +32,7 @@ if (isset($_GET['Team'])) {
 }
 elseif (isset($_GET['leagueAbb'])) {
   $league = $_GET['leagueAbb'];
-  $sql = "SELECT * from Matches m JOIN Teams t1 ON m.HomeTeam=t1.Club JOIN Teams t2 ON m.AwayTeam=t2.Club where HomeTeam='$league' OR AwayTeam='$league'";
+  $sql = "SELECT * from Matches m JOIN Teams t1 ON m.HomeTeam=t1.Club JOIN Teams t2 ON m.AwayTeam=t2.Club where t1.League='$league' OR t2.League='$league'";
 }
 else {
   $sql = "SELECT * from Matches";

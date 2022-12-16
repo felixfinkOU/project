b08f9978fe2ce392034e3f9f832592f121a150ba
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-12-15 19:17:10 -0600

    change name of dropdown item

--- header.php
+++ header.php
@@ -52,9 +52,8 @@
                   <li>
                     
                     <form method="get" action="teams.php">
-                      <label for="League" class="form-label"></label>
+                      <label for="League" class="form-label"><?=$leagueRow["Name"]?></label>
                       <input type="hidden" name="leagueAbb" value="<?=$leagueRow["Abbreviation"]?>">                 
-                      <input type="submit" class="btn btn-primary" value="Submit">
                     </form>
                   </li>
                 <?php

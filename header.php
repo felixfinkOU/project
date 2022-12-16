0543140d7c9ea12e51ab4adf218204d3572c7ea9
Felix Fink <Jan.Felix.Fink-1@ou.edu>
2022-12-15 19:22:47 -0600

    test

--- header.php
+++ header.php
@@ -52,8 +52,9 @@
                   <li>
                     
                     <form method="get" action="teams.php">
-                      <li><a class="dropdown-item"><?=$leagueRow["Name"]?></a></li>
+                      <label for="League" class="form-label"><?=$leagueRow["Name"]?></label>
                       <input type="hidden" name="leagueAbb" value="<?=$leagueRow["Abbreviation"]?>">                 
+                      <input type="submit" class="btn btn-primary" value="Submit">
                     </form>
                   </li>
                 <?php
